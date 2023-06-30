9b7068ddaad3c728f756153cb765de3656d8e612
Rajesh01-star <rajeshbehera489@gmail.com>
2023-06-30 22:28:01 +0530

    images added archive done

--- public/js/archive.js
+++ public/js/archive.js
@@ -1,28 +1,22 @@
 const roadTrip = [
-    "http://0x0.st/opZU.jpg",
-    "http://0x0.st/HQDU.jpg"
+    "http://0x0.st/HQ7i.jpg","http://0x0.st/HQ7-.jpg","http://0x0.st/HQDU.jpg","http://0x0.st/HQD0.jpg","http://0x0.st/HQ7o.jpg","http://0x0.st/HQ7H.jpg","http://0x0.st/HQ7X.jpg","http://0x0.st/HQ78.jpg","http://0x0.st/HQ7K.jpg","http://0x0.st/HQ7P.jpg","http://0x0.st/HQ7Z.jpg","http://0x0.st/HQ7N.jpg","http://0x0.st/HQ7q.jpg","http://0x0.st/HQ7b.jpg","http://0x0.st/HQ7c.jpg","http://0x0.st/HQ7T.jpg","http://0x0.st/HQ7A.jpg","http://0x0.st/HQ7m.jpg","http://0x0.st/HQ7a.jpg","http://0x0.st/HQ7B.jpg","http://0x0.st/HQ7M.jpg","http://0x0.st/HQDd.jpg","http://0x0.st/HQ7u.jpg","http://0x0.st/HQ7S.jpg","http://0x0.st/HQ7Q.jpg","http://0x0.st/HQ71.jpg","http://0x0.st/HQ7j.jpg","http://0x0.st/HQ7e.jpg","http://0x0.st/HQ72.MP.jpg","http://0x0.st/HQ7_.MP.jpg","http://0x0.st/HQ7L.MP.jpg","http://0x0.st/HQ79.MP.jpg","http://0x0.st/HQ7p.MP.jpg","http://0x0.st/HQD5.MP.jpg"
   ];
   
-  const esplanade = [
-    "http://0x0.st/HQD0.jpg",
-    "http://0x0.st/opN6.jpg",
-    "http://0x0.st/HQD0.jpg",
-    "http://0x0.st/opN6.jpg",
-    "http://0x0.st/HQD0.jpg",
-    "http://0x0.st/opN6.jpg",
-    "http://0x0.st/HQD0.jpg",
-    "http://0x0.st/opN6.jpg",
-    "http://0x0.st/HQD0.jpg",
-    "http://0x0.st/opN6.jpg",
-    "http://0x0.st/HQD0.jpg",
-    "http://0x0.st/opN6.jpg",
-    "http://0x0.st/HQD0.jpg",
-    "http://0x0.st/opN6.jpg",
-    "http://0x0.st/HQD0.jpg",
-    "http://0x0.st/opN6.jpg",
-    "http://0x0.st/HQD0.jpg",
-    "http://0x0.st/opN6.jpg"
+  
+  const esplanade = ["http://0x0.st/opNZ.jpg","http://0x0.st/opNN.jpg","http://0x0.st/opNq.jpg","http://0x0.st/opNb.jpg","http://0x0.st/opNc.jpg","http://0x0.st/opNT.jpg","http://0x0.st/opNA.jpg","http://0x0.st/opNm.jpg","http://0x0.st/opNa.jpg","http://0x0.st/opNM.jpg","http://0x0.st/opNS.jpg","http://0x0.st/opNQ.jpg","http://0x0.st/opNj.jpg","http://0x0.st/opNe.jpg","http://0x0.st/opN_.jpg","http://0x0.st/opN9.jpg","http://0x0.st/opNp.jpg","http://0x0.st/opNf.jpg","http://0x0.st/opNO.jpg","http://0x0.st/opNW.jpg","http://0x0.st/opN4.jpg","http://0x0.st/opNJ.jpg","http://0x0.st/opNy.jpg","http://0x0.st/opNt.jpg","http://0x0.st/opNv.jpg","http://0x0.st/opNw.jpg","http://0x0.st/opNx.jpg","http://0x0.st/opN3.jpg","http://0x0.st/opNY.jpg","http://0x0.st/opNg.jpg","http://0x0.st/opNE.jpg","http://0x0.st/opN6.jpg","http://0x0.st/opNI.jpg","http://0x0.st/opNl.jpg","http://0x0.st/opNU.jpg","http://0x0.st/opN0.jpg","http://0x0.st/opNG.jpg","http://0x0.st/opND.jpg","http://0x0.st/opNk.jpg","http://0x0.st/opNd.jpg","http://0x0.st/opNn.jpg","http://0x0.st/opN5.jpg","http://0x0.st/opNR.jpg","http://0x0.st/opN7.jpg","http://0x0.st/opNh.jpg","http://0x0.st/opNF.jpg","http://0x0.st/opNC.jpg","http://0x0.st/opqr.jpg","http://0x0.st/opqs.jpg","http://0x0.st/opqz.jpg","http://0x0.st/opqi.jpg","http://0x0.st/opq-.jpg","http://0x0.st/opqH.jpg","http://0x0.st/opqX.jpg","http://0x0.st/opq8.jpg","http://0x0.st/opqK.jpg","http://0x0.st/opqP.jpg","http://0x0.st/opqZ.jpg","http://0x0.st/opqN.jpg","http://0x0.st/opqq.jpg","http://0x0.st/opqb.jpg","http://0x0.st/opqc.jpg","http://0x0.st/opqT.jpg","http://0x0.st/opqA.jpg","http://0x0.st/opqm.jpg","http://0x0.st/opqa.jpg","http://0x0.st/opqB.jpg","http://0x0.st/opqM.jpg","http://0x0.st/opqu.jpg","http://0x0.st/opqS.jpg","http://0x0.st/opqQ.jpg","http://0x0.st/opq1.jpg","http://0x0.st/opqj.jpg","http://0x0.st/opqe.jpg","http://0x0.st/opq2.jpg","http://0x0.st/opq_.jpg","http://0x0.st/opqL.jpg","http://0x0.st/opq9.jpg","http://0x0.st/opqp.jpg","http://0x0.st/opqf.jpg","http://0x0.st/opqO.jpg","http://0x0.st/opqV.jpg","http://0x0.st/opqW.jpg"
   ];
+
+  const fest = ["http://0x0.st/HQnw.jpg","http://0x0.st/HQnx.jpg","http://0x0.st/HQn3.jpg","http://0x0.st/HQnY.jpg","http://0x0.st/HQng.jpg","http://0x0.st/HQnE.jpg","http://0x0.st/HQn6.jpg","http://0x0.st/HQnI.jpg","http://0x0.st/HQnl.jpg","http://0x0.st/HQnU.jpg","http://0x0.st/HQn0.jpg","http://0x0.st/HQnG.jpg","http://0x0.st/HQnD.jpg","http://0x0.st/HQnk.jpg","http://0x0.st/HQDG.jpg","http://0x0.st/HQnd.jpg","http://0x0.st/HQnn.jpg","http://0x0.st/HQn5.jpg","http://0x0.st/HQnR.jpg","http://0x0.st/HQn7.jpg","http://0x0.st/HQnh.jpg","http://0x0.st/HQnF.MP.jpg","http://0x0.st/HQnC.MP.jpg","http://0x0.st/HQ5r.MP.jpg","http://0x0.st/HQ5s.MP.jpg","http://0x0.st/HQ5z.MP.jpg","http://0x0.st/HQ5i.MP.jpg","http://0x0.st/HQ5-.jpg","http://0x0.st/HQ5o.jpg","http://0x0.st/HQ5H.MP.jpg","http://0x0.st/HQ5X.MP.jpg","http://0x0.st/HQ58.MP.jpg","http://0x0.st/HQ5K.MP.jpg","http://0x0.st/HQ5P.MP.jpg","http://0x0.st/HQ5Z.MP.jpg","http://0x0.st/HQ5N.jpg","http://0x0.st/HQ5q.jpg","http://0x0.st/HQ5b.jpg","http://0x0.st/HQ5c.jpg","http://0x0.st/HQ5T.jpg","http://0x0.st/HQ5A.jpg","http://0x0.st/HQ5m.jpg","http://0x0.st/HQ5a.jpg","http://0x0.st/HQ5B.jpg","http://0x0.st/HQ5M.jpg","http://0x0.st/HQ5u.jpg","http://0x0.st/HQ5S.jpg","http://0x0.st/HQ5Q.jpg","http://0x0.st/HQDD.jpg","http://0x0.st/HQ51.jpg","http://0x0.st/HQ5j.jpg","http://0x0.st/HQ5e.jpg","http://0x0.st/HQ52.MP.jpg"
+  ];
+
+  const holi = [
+    "http://0x0.st/HQ5L.jpg","http://0x0.st/HQ59.jpg","http://0x0.st/HQ5p.jpg","http://0x0.st/HQ5f.jpg","http://0x0.st/HQ5O.jpg","http://0x0.st/HQ5V.jpg","http://0x0.st/HQ5W.jpg","http://0x0.st/HQ54.jpg","http://0x0.st/HQ5J.jpg","http://0x0.st/HQ5y.jpg","http://0x0.st/HQ5t.jpg","http://0x0.st/HQ5v.jpg","http://0x0.st/HQ5w.MP.jpg","http://0x0.st/HQ5x.MP.jpg","http://0x0.st/HQ53.MP.jpg","http://0x0.st/HQ5Y.MP.jpg","http://0x0.st/HQ5g.jpg","http://0x0.st/HQ5E.jpg","http://0x0.st/HQ56.jpg","http://0x0.st/HQ5I.jpg","http://0x0.st/HQ5l.jpg","http://0x0.st/HQ5U.jpg","http://0x0.st/HQ50.jpg","http://0x0.st/HQ5G.jpg","http://0x0.st/HQ5D.jpg#nsfw","http://0x0.st/HQ5k.jpg","http://0x0.st/HQ5d.jpg","http://0x0.st/HQ5n.jpg","http://0x0.st/HQ55.jpg","http://0x0.st/HQ5R.jpg","http://0x0.st/HQ57.jpg","http://0x0.st/HQ5h.jpg","http://0x0.st/HQ5F.jpg","http://0x0.st/HQ5C.jpg","http://0x0.st/HQRr.jpg","http://0x0.st/HQRs.jpg","http://0x0.st/HQRz.jpg","http://0x0.st/HQRi.jpg","http://0x0.st/HQR-.jpg","http://0x0.st/HQRo.jpg","http://0x0.st/HQRH.jpg","http://0x0.st/HQRX.jpg","http://0x0.st/HQR8.jpg","http://0x0.st/HQRK.jpg","http://0x0.st/HQRP.jpg#nsfw","http://0x0.st/HQRZ.jpg","http://0x0.st/HQRN.jpg","http://0x0.st/HQRq.jpg","http://0x0.st/HQRb.jpg","http://0x0.st/HQRc.jpg","http://0x0.st/HQRT.jpg","http://0x0.st/HQRA.jpg","http://0x0.st/HQRm.jpg","http://0x0.st/HQRa.jpg","http://0x0.st/HQRB.jpg","http://0x0.st/HQRM.jpg","http://0x0.st/HQRu.jpg","http://0x0.st/HQRS.jpg","http://0x0.st/HQRQ.jpg","http://0x0.st/HQR1.jpg","http://0x0.st/HQRe.jpg","http://0x0.st/HQRO.MP.jpg","http://0x0.st/HQRV.MP.jpg","http://0x0.st/HQRW.MP.jpg","http://0x0.st/HQR4.MP.jpg","http://0x0.st/HQRJ.MP.jpg","http://0x0.st/HQRy.MP.jpg","http://0x0.st/HQRt.MP.jpg","http://0x0.st/HQRv.MP.jpg","http://0x0.st/HQRw.MP.jpg","http://0x0.st/HQRx.MP.jpg","http://0x0.st/HQR3.MP~2.jpg","http://0x0.st/HQRY.MP.jpg","http://0x0.st/HQRg.MP.jpg","http://0x0.st/HQRE.MP.jpg","http://0x0.st/HQR6.MP.jpg","http://0x0.st/HQRI.MP.jpg","http://0x0.st/HQRl.jpg","http://0x0.st/HQRU.jpg","http://0x0.st/HQR0.jpg","http://0x0.st/HQRG.MP.jpg","http://0x0.st/HQRD.MP.jpg","http://0x0.st/HQRk.MP.jpg","http://0x0.st/HQRd.MP.jpg","http://0x0.st/HQRn.MP.jpg","http://0x0.st/HQR5.MP.jpg","http://0x0.st/HQRR.MP.jpg","http://0x0.st/HQR7.MP.jpg","http://0x0.st/HQRh.MP.jpg"
+  ];
+
+  const library = [
+    "http://0x0.st/opZg.jpg","http://0x0.st/opZE.jpg","http://0x0.st/opZ6.jpg","http://0x0.st/opZI.jpg","http://0x0.st/opZl.jpg","http://0x0.st/opZU.jpg","http://0x0.st/opZ0.jpg","http://0x0.st/opZG.jpg","http://0x0.st/opZD.jpg","http://0x0.st/opZk.jpg","http://0x0.st/opZd.jpg","http://0x0.st/opZn.jpg","http://0x0.st/opZ5.jpg","http://0x0.st/opZR.jpg","http://0x0.st/opZ7.jpg","http://0x0.st/opZh.jpg","http://0x0.st/opZF.jpg","http://0x0.st/opZC.jpg","http://0x0.st/opNr.jpg","http://0x0.st/opNs.jpg","http://0x0.st/opNz.jpg","http://0x0.st/opNi.jpg","http://0x0.st/opN-.jpg","http://0x0.st/opNo.jpg","http://0x0.st/opNH.jpg","http://0x0.st/opNX.jpg","http://0x0.st/opN8.jpg","http://0x0.st/opNK.jpg"
+  ];
+  
   
   const showCaseArr = window.location.href.split('/').pop();
   
@@ -34,7 +28,13 @@ const roadTrip = [
       imageArray = roadTrip;
     } else if (showCaseArr === "esplanade") {
       imageArray = esplanade;
-    } else {
+    } else if(showCaseArr === "fest") {
+      imageArray = fest;
+    }else if(showCaseArr === "holi") {
+        imageArray = holi;
+    }else if(showCaseArr === "library") {
+        imageArray = library;
+      }else {
       // Handle the case when showCaseArr doesn't match any specific array
       // For example, display a default set of images or show an error message
       return;
